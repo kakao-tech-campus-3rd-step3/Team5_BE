e3f80fa424a287d3b85bda2da121e6f8be0ecf45
kimjhyun0627 <kimjhyun0627@gmail.com>
2025-08-27 23:18:37 +0900

    feat: 프로젝트 기본 셋업
    - Gradle 빌드
    - java 21
    - dependency는 아직 최소화
    - 실행 체크 부탁드립니다
    
    Res: #3

---
+++ dailyq/src/main/java/com/knuissant/dailyq/DailyQApplication.java
@@ -0,0 +1,13 @@
+package com.knuissant.dailyq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyQApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyQApplication.class, args);
+	}
+
+}
