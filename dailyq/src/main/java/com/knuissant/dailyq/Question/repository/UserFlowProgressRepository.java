186500160c94cea681d870bad9acd68371dd29f0
kimjhyun0627 <kimjhyun0627@gmail.com>
2025-09-17 01:15:51 +0900

    fix: 불필요한 파일 삭제

--- dailyq/src/main/java/com/knuissant/dailyq/Question/repository/UserFlowProgressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.knuissant.dailyq.Question.repository;
-
-import com.knuissant.dailyq.Question.entity.UserFlowProgress;
-import com.knuissant.dailyq.Question.entity.UserFlowProgressId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserFlowProgressRepository extends JpaRepository<UserFlowProgress, UserFlowProgressId> {}
-
-
--- dailyq/src/main/resources/sql/data_jobs.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO jobs(name) VALUES
-  ('FRONTEND'),
-  ('BACKEND'),
-  ('IOS'),
-  ('ANDROID'),
-  ('DATA')
-ON DUPLICATE KEY UPDATE name = VALUES(name);
-
-
