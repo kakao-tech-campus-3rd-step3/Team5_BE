9a2f38366e1f2cccc704e77590555bb8ee4f4e51
hayun <psh040826@naver.com>
2025-09-19 05:00:17 +0900

    feat : DTO password 추가

--- dailyq/src/main/java/com/knuissant/dailyq/dto/users/UserCreateRequest.java
+++ dailyq/src/main/java/com/knuissant/dailyq/dto/users/UserCreateRequest.java
@@ -10,6 +10,10 @@ public record UserCreateRequest(
         @Email(message = "올바른 이메일 형식이 아닙니다.")
         String email,
 
+        @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
+        @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
+        String password,
+
         @NotBlank(message = "이름은 필수 입력 항목입니다.")
         @Size(max = 100, message = "이름은 100자를 초과할 수 없습니다.")
         String name
