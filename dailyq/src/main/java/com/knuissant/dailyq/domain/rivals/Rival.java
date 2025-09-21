ecd854b6b3a42427f38db9f0f63650b79a9f7c9e
Dohyun <sks020k@naver.com>
2025-09-22 00:50:28 +0900

    refactor : receiver_id을 기준으로 조회할 때 인덱스를 활용하도록 수정 - RivalService의 getReceivedRequests가 receiverId를 기준으로 조회

--- dailyq/src/main/java/com/knuissant/dailyq/domain/rivals/Rival.java
+++ dailyq/src/main/java/com/knuissant/dailyq/domain/rivals/Rival.java
@@ -8,6 +8,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -34,6 +35,9 @@ import com.knuissant.dailyq.exception.ErrorCode;
                         name = "uq_rivals_pair",
                         columnNames = {"sender_id", "receiver_id"}
                 )
+        },
+        indexes = {
+        @Index(name = "idx_rivals_receiver", columnList = "receiver_id")
         }
 )
 public class Rival {
