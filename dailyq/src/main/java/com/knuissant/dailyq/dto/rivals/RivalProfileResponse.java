c1d755a6c9a826f3f3866b02b3d809867111cc4f
Dohyun <sks020k@naver.com>
2025-09-24 21:15:40 +0900

    refactor : RivalProfileResponse 코드 컨벤션에 맞게 수정 - long -> Long , int -> Integer

--- dailyq/src/main/java/com/knuissant/dailyq/dto/rivals/RivalProfileResponse.java
+++ dailyq/src/main/java/com/knuissant/dailyq/dto/rivals/RivalProfileResponse.java
@@ -3,11 +3,19 @@ package com.knuissant.dailyq.dto.rivals;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.knuissant.dailyq.domain.users.User;
+
 public record RivalProfileResponse(
         String name,
-        int streak,
-        long totalAnswerCount,
+        Integer streak,
+        Long totalAnswerCount,
         List<LocalDate> datesForStreak
 ) {
-
+    public static RivalProfileResponse from(User user, long totalAnswerCount, List<LocalDate> datesForStreak) {
+        return new RivalProfileResponse(
+                user.getName(),
+                user.getStreak(),
+                totalAnswerCount,
+                datesForStreak
+        );
 }
