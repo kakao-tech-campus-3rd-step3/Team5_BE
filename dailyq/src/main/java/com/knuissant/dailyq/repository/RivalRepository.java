129d327918a72308bbc9630e2322f57554a854f4
Dohyun <sks020k@naver.com>
2025-09-26 10:09:48 +0900

    feat : 커서 기반 조회 메서드 추가

--- dailyq/src/main/java/com/knuissant/dailyq/repository/RivalRepository.java
+++ dailyq/src/main/java/com/knuissant/dailyq/repository/RivalRepository.java
@@ -3,7 +3,11 @@ package com.knuissant.dailyq.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.knuissant.dailyq.domain.rivals.Rival;
 
@@ -16,4 +20,11 @@ public interface RivalRepository extends JpaRepository<Rival, Long> {
     List<Rival> findAllBySenderId(Long senderId);
 
     List<Rival> findAllByReceiverId(Long receiverId);
+
+    @Query("SELECT r FROM Rival r WHERE r.sender.id = :senderId AND r.id > :lastId ORDER BY r.id ASC")
+    Slice<Rival> findBySenderIdAndIdGreaterThan(@Param("senderId") Long senderId,
+            @Param("lastId") Long lastId, Pageable pageable);
+
+    // 첫 페이지 조회
+    Slice<Rival> findAllBySenderId(Long senderId, Pageable pageable);
 }
