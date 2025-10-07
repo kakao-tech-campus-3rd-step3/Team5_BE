d98f3d53913d9bce5b19b938f775e361b1a414a4
kimjhyun0627 <kimjhyun0627@gmail.com>
2025-10-08 00:37:24 +0900

    refactor: 비동기 처리 제거

--- dailyq/src/main/java/com/knuissant/dailyq/DailyQApplication.java
+++ dailyq/src/main/java/com/knuissant/dailyq/DailyQApplication.java
@@ -3,10 +3,8 @@ package com.knuissant.dailyq;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableRetry
-@EnableAsync
 @SpringBootApplication
 public class DailyQApplication {
 
