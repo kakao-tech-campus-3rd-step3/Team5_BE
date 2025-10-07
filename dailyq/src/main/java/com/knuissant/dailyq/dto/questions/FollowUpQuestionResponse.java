35a7ba09803561648488a26cda4879a57871b661
kimjhyun0627 <kimjhyun0627@gmail.com>
2025-10-08 00:45:23 +0900

    refactor: 꼬리질문 Response DTO record로 변경

--- dailyq/src/main/java/com/knuissant/dailyq/dto/questions/FollowUpQuestionResponse.java
+++ dailyq/src/main/java/com/knuissant/dailyq/dto/questions/FollowUpQuestionResponse.java
@@ -2,17 +2,7 @@ package com.knuissant.dailyq.dto.questions;
 
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class FollowUpQuestionResponse {
-
-    @JsonProperty("questions")
-    private List<String> questions;
+public record FollowUpQuestionResponse(
+    List<String> questions
+) {
 }
