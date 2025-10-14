98c8d77133cde88d0313ad5bc393f3d109982b73
Junhee <dkfl7190@naver.com>
2025-10-14 21:18:05 +0900

    refactor: RestClient.Builder 빈등록 -> OpenAiApi 빈등록

--- dailyq/src/main/java/com/knuissant/dailyq/config/OpenAiConfig.java
+++ dailyq/src/main/java/com/knuissant/dailyq/config/OpenAiConfig.java
@@ -1,6 +1,9 @@
 package com.knuissant.dailyq.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
@@ -10,16 +13,26 @@ import org.springframework.web.client.RestClient;
 public class OpenAiConfig {
 
     @Bean
-    public ChatClient chatClient(ChatClient.Builder builder) {
-        return builder.build();
-    }
-
-    @Bean
-    public RestClient.Builder restClientBuilder() {
+    public OpenAiApi openAiApi(@Value("${spring.ai.openai.api-key}") String apiKey) {
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
         factory.setConnectTimeout(5000);
         factory.setReadTimeout(10000);
 
-        return RestClient.builder().requestFactory(factory);
+        RestClient.Builder restClientBuilder = RestClient.builder().requestFactory(factory);
+
+        return OpenAiApi.builder()
+                .apiKey(apiKey)
+                .restClientBuilder(restClientBuilder)
+                .build();
+    }
+
+    @Bean
+    public ChatClient chatClient(OpenAiApi openAiApi) {
+
+        OpenAiChatModel chatModel = OpenAiChatModel.builder()
+                .openAiApi(openAiApi)
+                .build();
+
+        return ChatClient.builder(chatModel).build();
     }
 }
