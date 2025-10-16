1c08a050dca81868fa4ef2663fc156e2b152184d
Junhee <dkfl7190@naver.com>
2025-10-16 19:09:54 +0900

    refactor: BusinessException, InfraExceptiion 생성자 통일
    - 기존: 가변인자를 받는 생성자와 받지 않는 생성자 분리
    - 현재: 하나로 통일하여 가변인자가 없는 경우, 컴파일러가 자동으로 빈 배열을 만들어 전달하도록 함

--- dailyq/src/main/java/com/knuissant/dailyq/exception/InfraException.java
+++ dailyq/src/main/java/com/knuissant/dailyq/exception/InfraException.java
@@ -8,12 +8,6 @@ public class InfraException extends RuntimeException {
     private final ErrorCode errorCode;
     private final Object[] args;
 
-    public InfraException(ErrorCode errorCode) {
-        super(errorCode.getMessage());
-        this.errorCode = errorCode;
-        this.args = new Object[]{};
-    }
-
     public InfraException(ErrorCode errorCode, Object... args) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
