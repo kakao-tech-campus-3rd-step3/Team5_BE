69de208cccb2c7d53e5506075a82676fd6ca229f
Junhee <dkfl7190@naver.com>
2025-10-20 20:51:36 +0900

    feat: NCP CLOVA Speech 연동 설정

--- dailyq/src/main/java/com/knuissant/dailyq/config/NcpConfig.java
+++ dailyq/src/main/java/com/knuissant/dailyq/config/NcpConfig.java
@@ -3,6 +3,8 @@ package com.knuissant.dailyq.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestClient;
 
 import lombok.Getter;
 
@@ -27,12 +29,29 @@ public class NcpConfig {
     @Value("${ncp.storage.bucket-name}")
     private String bucketName;
 
+    @Value("${ncp.clova.stt.invoke-url}")
+    private String clovaInvokeUrl;
+    @Value("${ncp.clova.stt.secret-key}")
+    private String clovaSecretKey;
+    @Value("${ncp.clova.stt.callback-server}")
+    private String clovaCallbackServer;
+
     // Object Storage
     @Bean
     public AmazonS3 ncpObjectStorageClient() {
         return AmazonS3ClientBuilder.standard()
                 .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(storageEndpoint, storageRegion))
                 .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(storageAccessKey, storageSecretKey)))
+                .build();
+    }
+
+    // CLOVA speech
+    @Bean
+    public RestClient ncpClovaRestClient() {
+        return RestClient.builder()
+                .baseUrl(clovaInvokeUrl)
+                .defaultHeader("Accept", MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader("X-CLOVASPEECH-API-KEY", clovaSecretKey)
                 .build();
     }
 }
