4a2aff3e392a5678c5811954ef3259b5ffb1dbc6
Junhee <dkfl7190@naver.com>
2025-11-04 20:18:38 +0900

    feat: createdAt, updatedAt 관련 base entity 생성
    - insertabla = false, updatable = false 설정 -> DB 기반 타임 설정

---
+++ dailyq/src/main/java/com/knuissant/dailyq/domain/common/BaseCreatedAtEntity.java
@@ -0,0 +1,16 @@
+package com.knuissant.dailyq.domain.common;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+public class BaseCreatedAtEntity {
+
+    @Column(name = "created_at", nullable = false, insertable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
